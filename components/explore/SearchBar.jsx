dd69c7bb72ae2305614e284ec4089ffcfae85c94
Aaron Owusu <34800051+aaronowusu@users.noreply.github.com>
2023-04-03 16:14:51 +0100

    feature: Addeing  the explore header, which includes the twitter logo, search bar and menu (#1)

---
+++ components/explore/SearchBar.jsx
@@ -0,0 +1,48 @@
+import { HiOutlineSearch } from "react-icons/hi";
+import { useState, useRef } from "react";
+
+const SearchBar = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(inputRef.current.value);
+
+    // Replace main content with hard-coded text
+  };
+
+  return (
+    <form onSubmit={submitHandler} >
+      <div
+        className={`nav__searchbar w-full flex items-center rounded-3xl border bg-search-bg-color-light dark:bg-search-bg-color-dark ${
+          isFocused ? "border-blue-500" : "border dark:border-black"
+        }`}
+      >
+        <HiOutlineSearch
+          className={`w-5 h-5  mx-2 ${
+            isFocused ? "stroke-blue-500" : "stroke-search-text-color"
+          }`}
+        />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          ref={inputRef}
+          className="w-full py-[4px] px-[12px] text-black dark:text-white bg-transparent focus:outline-none"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
