ed2241dc79351a4c0653884fe7d0119b99e076a9
Aaron Owusu <34800051+aaronowusu@users.noreply.github.com>
2023-04-11 15:39:23 +0100

    fixed sticky nav and started work on sidebar (#7)

--- components/explore/Sidebar.jsx
+++ components/explore/Sidebar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import Logo from "./SidebarLogo";
+import { IoSettingsOutline } from "react-icons/io5";
 const Sidebar = () => {
   return (
     <>
-      <div className="hidden md:flex md:flex-[0.5] xl:flex-2 h-screen">Sidebar</div>
+      <div className="hidden md:flex md:flex-[0.5] xl:flex-2 h-screen flex-row justify-center">
+        <div className="sidebar__wrapper flex flex-col space-y-4">
+          <div className="logo pt-4">
+            <Logo />
+          </div>
+          <div className="explore"></div>
+          <div className="settings">
+            <IoSettingsOutline className="dark:stroke-white stroke-black w-full h-full" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
