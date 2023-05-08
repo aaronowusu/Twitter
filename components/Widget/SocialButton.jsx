9fe1ffaa59e7008479b5ba64713d4572b3e52148
Aaron Owusu <34800051+aaronowusu@users.noreply.github.com>
2023-05-08 17:28:25 +0100

    adding widget for desktop (#10)

---
+++ components/Widget/SocialButton.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import React from 'react';
+
+function SocialButton(props) {
+  return (
+    <button className=' rounded-full lg:w-full  bg-white text-black  px-2 py-1 dark:hover:opacity-80 hover:bg-[#E6E6E6] my-1 border border-[#EFF0F2]'>
+      <div className='flex flex-row justify-center items-center'>
+        <Image className='mx-2' src={props.src} width={30} height={30} alt='Social Login logo' />
+        {props.children}
+      </div>
+    </button>
+  );
+}
+
+export default SocialButton;
---
+++ components/Widget/WidgetFooter.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const WidgetFooter = () => {
+  return (
+    <div className='tos mb-4 ml-6 lg:w-[280px] xl:w-[370px]'>
+      <nav className='flex flex-col items-center justify-center text-gray-500 text-xs'>
+        <div className='flex flex-row space-x-2'>
+          <a href='#' className=' hover:underline'>
+            <span>Terms of Service</span>
+          </a>
+          <a href='#' className='hover:underline'>
+            <span>Privacy Policy</span>
+          </a>
+          <a href='#' className=' hover:underline'>
+            <span>Cookie Policy</span>
+          </a>
+        </div>
+
+        <div className='flex flex-row space-x-2'>
+          <a href='#' className='hover:underline'>
+            <span>Accessibility</span>
+          </a>
+          <a href='#' className='hover:underline '>
+            <span>Ads info</span>
+          </a>
+          <a href='#' className=' hover:underline'>
+            <span>More ...</span>
+          </a>
+        </div>
+
+        <a href='#' className=' hover:underline'>
+          © 2023 X Corp.
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default WidgetFooter;
